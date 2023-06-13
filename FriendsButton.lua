4e2dafd578960c01ed3db9e2867fb393b1472944
Mr_Rosh <cosminrosca1995@gmail.com>
2023-06-13 19:53:15 +0300

    Initial commit

---
+++ FriendsButton.lua
@@ -0,0 +1,18 @@
+function bFriendsButton_OnLoad()
+	this:RegisterEvent("VARIABLES_LOADED");
+	this:RegisterEvent("FRIENDLIST_UPDATE");
+
+	-- if UnitName("player") == "Bonho" or UnitName("player") == "Braindamage"
+	-- 	then FriendsButtonFrame:Hide(); 
+	-- end
+end
+
+function GetFriendsOnline()
+	local numberOfFriends = GetNumFriends();
+	local fCount = 0;
+	for i = 1, numberOfFriends, 1 do
+		local name, level, class, loc, connected, status = GetFriendInfo(i);
+		if connected == 1 then fCount = fCount + 1; end
+	end
+	return fCount;
+end
